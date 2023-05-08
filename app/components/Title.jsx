c23da2495c8691a7ded8c0f557c18c096e5eea42
yen0224 <yen0224@proton.me>
2023-05-08 15:51:52 +0800

    complete game

--- app/components/Title.jsx
+++ app/components/Title.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = ({ content }) => {
   return (
-    <div className="mt-[3vh] pl-[7%] h-[5vh]">
+    <div className="w-[20vw]">
       <h1 className="text-4xl">{content}</h1>
     </div>
   );
